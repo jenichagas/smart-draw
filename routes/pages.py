f9a6744884df86dc6bc80671636db18bfbbea20d
Jeniffer <jenif.work@gmail.com>
2025-04-11 17:54:13 -0300

    🐛 fix a bug

--- routes/pages.py
+++ routes/pages.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from services.storage import load_participants
+from services.storage import save_participants
 import random
 
 router = APIRouter()
@@ -10,7 +10,7 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def smartdraw_home(request: Request):
-    participants = load_participants()
+    save_participants([])
     return templates.TemplateResponse(
         "app/smartdraw.html", {"request": request, "participants": []}
     )
